41c7f5c08d49866f53f89e63d04566ad361ec201
januarsyawall <muhammad.aan46789@gmail.com>
2022-10-29 21:18:21 +0700

    tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,17 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Januar</li>
+        <li>Nama Belakang : Syawalludin </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Tebu</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+    </ul>
+</section>
